f180ff76c57ee3a4060bf385ac599129cd64c1b8
Jared <jcknueven@gmail.com>
2016-06-24 11:52:33 -0400

    add js

--- js/github.js
+++ js/github.js
@@ -5,7 +5,6 @@ $.ajax({
 		type:"GET",
 		success: function(users)
 		{
-			console.log();
 			var avatar = users.avatar_url;
 			var bio = users.bio;
 			var name = users.name;
@@ -14,15 +13,34 @@ $.ajax({
 			var followers = users.followers;
 			var following = users.following;
 			var create = users.created_at;
+			var time = moment(create).format('MMM,DD YYYY');
 
 			$('.profileImg').append("<img src='images/Jared_profile3.jpg'>");
 			$('.name').append("<h2>"+name+"</h2>");
 			$('.username').append("<h3>"+username+"</h3>");
 			$('.bio').append("<p>"+bio+"</p>");
-			$('.started').append("<img src='images/clock.svg'><p>Joined"+create+"</p>");
+			$('.started').append("<img src='images/clock.svg'><p>Joined on "+time+"</p>");
 			$('.stats').append("<a class='vcard-stat'><strong class='vcard-stat-count d-block'>"+followers+"</strong><span>Followers</span></a><a class='vcard-stat'><strong class='vcard-stat-count d-block'>0</strong><span>Starred</span></a><a class='vcard-stat'><strong class='vcard-stat-count d-block'>"+following+"</strong><span>Following</span></a>");
 
 		}
 	});
+
+$.ajax({
+	url: "https://api.github.com/users/jknueven/repos",
+	type: "GET",
+	success: function(repos)
+	{
+		console.log(repos);
+		var reposit = results.objects;
+
+
+		resposit.forEach(function(repositories)
+		{
+			var repoName = reposit.name;
+			var repoDescrip = reposit.description;
+			var update = reposit.updated_at;
+		})
+	}
+});
 
 });
